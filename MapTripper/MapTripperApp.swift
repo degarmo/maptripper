647b5f03862d703e0371770af1a0587e6ca7f1ad
Cory DeGarmo <degarmo@gmail.com>
2022-03-23 18:53:05 -0500

    Initial Commit

---
+++ MapTripper/MapTripperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTripperApp.swift
+//  MapTripper
+//
+//  Created by CD on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MapTripperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
